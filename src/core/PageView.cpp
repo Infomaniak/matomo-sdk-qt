55289edaf6fe6f8acc4df195b863bd2c5d3261a4
Romain Galland <romain.galland@infomaniak.com>
2026-06-03 17:10:18 +0200

    fix(core): enhance PageView::isValid to validate custom dimensions

--- src/core/PageView.cpp
+++ src/core/PageView.cpp
@@ -1,9 +1,12 @@
 #include <MatomoQt/PageView.h>
 
+#include <algorithm>
+
 namespace MatomoQt {
 
 bool PageView::isValid() const {
-    return !path.trimmed().isEmpty();
+    return !path.trimmed().isEmpty() && std::all_of(customDimensions.cbegin(), customDimensions.cend(),
+                                                    [](const CustomDimension &dimension) { return dimension.isValid(); });
 }
 
 } // namespace MatomoQt
