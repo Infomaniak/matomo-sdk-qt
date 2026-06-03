b16c09b55c38ba81d2bd361f3c67ff729cb42d26
Romain Galland <romain.galland@infomaniak.com>
2026-06-03 17:11:04 +0200

    test(tracker): expand TrackerTest to cover config, payload validity, and signal emission

--- tests/unit/tst_tracker.cpp
+++ tests/unit/tst_tracker.cpp
@@ -10,6 +10,8 @@
 
 #include <QtTest/QtTest>
 
+#include <limits>
+
 using namespace MatomoQt;
 
 namespace {
@@ -24,6 +26,10 @@ class TrackerTest : public QObject {
         static void trackerDoesNotAcceptWithoutConsentByDefault();
 
         static void trackerAcceptsCallsAfterConsent();
+        static void trackerRejectsInvalidConfigBeforePayload();
+        static void trackerRejectsInvalidPayloadAfterConsent();
+        static void trackerDoesNotEmitSignalsForUnchangedValues();
+        static void trackerEmitsConfigChangedForChangedConfig();
 
         static void trackerSupportsDisabledAndOptOutModes();
 };
@@ -43,26 +49,58 @@ void TrackerTest::defaultConfigIsPrivacySafe() {
 
 void TrackerTest::configValidityRequiresEndpointAndSiteId() {
     TrackerConfig config;
-    config.endpoint = QUrl(QStringLiteral("https://matomo.example.com/matomo.php"));
-
-    QVERIFY(!config.isValid());
+    config.endpoint = QUrl(QStringLiteral("matomo.example.com/matomo.php"));
 
     config.siteId = 1;
+    QVERIFY(!config.isValid());
+
+    config.endpoint = QUrl(QStringLiteral("file:///tmp/matomo.php"));
+    QVERIFY(!config.isValid());
+
+    config.endpoint = QUrl(QStringLiteral("https://matomo.example.com/matomo.php"));
     QVERIFY(config.isValid());
 }
 
 void TrackerTest::payloadValidityUsesRequiredFields() {
     QVERIFY(!PageView{}.isValid());
     QVERIFY(PageView{.path = QStringLiteral("preferences")}.isValid());
+    const PageView pageViewWithInvalidDimension{.path = QStringLiteral("preferences"), .customDimensions = {{.id = 0}}};
+    QVERIFY(!pageViewWithInvalidDimension.isValid());
+    const PageView pageViewWithValidDimension{.path = QStringLiteral("preferences"), .customDimensions = {{.id = 1}}};
+    QVERIFY(pageViewWithValidDimension.isValid());
 
     QVERIFY(!Event{}.isValid());
     QVERIFY(!Event{.category = QStringLiteral("preferences")}.isValid());
+    const Event eventWithNaN{.category = QStringLiteral("preferences"),
+                             .action = QStringLiteral("click"),
+                             .value = std::numeric_limits<double>::quiet_NaN()};
+    QVERIFY(!eventWithNaN.isValid());
+    const Event eventWithInfinity{.category = QStringLiteral("preferences"),
+                                  .action = QStringLiteral("click"),
+                                  .value = std::numeric_limits<double>::infinity()};
+    QVERIFY(!eventWithInfinity.isValid());
+    const Event eventWithInvalidDimension{
+            .category = QStringLiteral("preferences"),
+            .action = QStringLiteral("click"),
+            .customDimensions = {{.id = 0}},
+    };
+    QVERIFY(!eventWithInvalidDimension.isValid());
     const Event validEvent{.category = QStringLiteral("preferences"), .action = QStringLiteral("click")};
     QVERIFY(validEvent.isValid());
+    const Event eventWithValidOptionalFields{
+            .category = QStringLiteral("preferences"),
+            .action = QStringLiteral("click"),
+            .value = 0.0,
+            .customDimensions = {{.id = 1}},
+    };
+    QVERIFY(eventWithValidOptionalFields.isValid());
 
     QVERIFY(!CustomDimension{}.isValid());
     const CustomDimension validDimension{.id = 1, .value = QStringLiteral("enabled")};
     QVERIFY(validDimension.isValid());
+
+    QVERIFY(!RequestResult{}.accepted());
+    QCOMPARE(RequestResult{}.status, RequestResult::Status::InvalidConfig);
 }
 
 void TrackerTest::trackerDoesNotAcceptWithoutConsentByDefault() {
@@ -91,6 +129,75 @@ void TrackerTest::trackerAcceptsCallsAfterConsent() {
     QCOMPARE(tracker.trackEvent({.category = QStringLiteral("preferences"), .action = QStringLiteral("click")}).status,
              RequestResult::Status::Accepted);
     QCOMPARE(tracker.sendPing().status, RequestResult::Status::Accepted);
+}
+
+void TrackerTest::trackerRejectsInvalidConfigBeforePayload() {
+    Tracker tracker;
+    tracker.setConsentState(ConsentState::Granted);
+
+    QCOMPARE(tracker.trackPageView({}).status, RequestResult::Status::InvalidConfig);
+    QCOMPARE(tracker.trackEvent({}).status, RequestResult::Status::InvalidConfig);
+    QCOMPARE(tracker.sendPing().status, RequestResult::Status::InvalidConfig);
+}
+
+void TrackerTest::trackerRejectsInvalidPayloadAfterConsent() {
+    TrackerConfig config;
+    config.endpoint = QUrl(QStringLiteral("https://matomo.example.com/matomo.php"));
+    config.siteId = 1;
+
+    Tracker tracker(config);
+    tracker.setConsentState(ConsentState::Granted);
+
+    QCOMPARE(tracker.trackPageView({}).status, RequestResult::Status::InvalidPayload);
+    QCOMPARE(tracker.trackEvent({}).status, RequestResult::Status::InvalidPayload);
+    QCOMPARE(tracker.trackPageView({.path = QStringLiteral("preferences"), .customDimensions = {{.id = 0}}}).status,
+             RequestResult::Status::InvalidPayload);
+    QCOMPARE(tracker.trackEvent({.category = QStringLiteral("preferences"),
+                                 .action = QStringLiteral("click"),
+                                 .customDimensions = {{.id = 0}}})
+                     .status,
+             RequestResult::Status::InvalidPayload);
+    QCOMPARE(tracker.trackEvent({.category = QStringLiteral("preferences"),
+                                 .action = QStringLiteral("click"),
+                                 .value = std::numeric_limits<double>::quiet_NaN()})
+                     .status,
+             RequestResult::Status::InvalidPayload);
+}
+
+void TrackerTest::trackerDoesNotEmitSignalsForUnchangedValues() {
+    TrackerConfig config;
+    config.endpoint = QUrl(QStringLiteral("https://matomo.example.com/matomo.php"));
+    config.siteId = 1;
+
+    Tracker tracker(config);
+    tracker.setConsentState(ConsentState::Granted);
+
+    QSignalSpy configSpy(&tracker, &Tracker::configChanged);
+    QSignalSpy consentSpy(&tracker, &Tracker::consentStateChanged);
+    QSignalSpy enabledSpy(&tracker, &Tracker::enabledChanged);
+
+    tracker.setConfig(config);
+    tracker.setConsentState(ConsentState::Granted);
+    tracker.setEnabled(true);
+
+    QCOMPARE(configSpy.count(), 0);
+    QCOMPARE(consentSpy.count(), 0);
+    QCOMPARE(enabledSpy.count(), 0);
+}
+
+void TrackerTest::trackerEmitsConfigChangedForChangedConfig() {
+    TrackerConfig config;
+    config.endpoint = QUrl(QStringLiteral("https://matomo.example.com/matomo.php"));
+    config.siteId = 1;
+
+    Tracker tracker(config);
+    QSignalSpy configSpy(&tracker, &Tracker::configChanged);
+
+    config.privacyMode = PrivacyMode::ConsentExemptWithOptOut;
+    tracker.setConfig(config);
+
+    QCOMPARE(configSpy.count(), 1);
+    QCOMPARE(tracker.config().privacyMode, PrivacyMode::ConsentExemptWithOptOut);
 }
 
 void TrackerTest::trackerSupportsDisabledAndOptOutModes() {
