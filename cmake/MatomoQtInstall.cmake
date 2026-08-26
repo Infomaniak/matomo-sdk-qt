62fe53d084fdbdc5090003a4589278103227766d
Lawrence Leitgib <lawrence.leitgib@infomaniak.com>
2026-08-26 10:50:33 +0200

    feat(qml): add CI configuration for QML module

--- cmake/MatomoQtInstall.cmake
+++ cmake/MatomoQtInstall.cmake
@@ -24,34 +24,6 @@ install(
     INCLUDES DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}"
 )
 
-if(TARGET MatomoQtQml)
-    install(
-        TARGETS MatomoQtQml
-        EXPORT MatomoQtTargets
-        RUNTIME DESTINATION "${CMAKE_INSTALL_LIBDIR}/qml/MatomoQt"
-        LIBRARY DESTINATION "${CMAKE_INSTALL_LIBDIR}/qml/MatomoQt"
-        ARCHIVE DESTINATION "${CMAKE_INSTALL_LIBDIR}/qml/MatomoQt"
-    )
-
-    if(TARGET MatomoQtQmlPlugin)
-        install(
-            TARGETS MatomoQtQmlPlugin
-            EXPORT MatomoQtTargets
-            RUNTIME DESTINATION "${CMAKE_INSTALL_LIBDIR}/qml/MatomoQt"
-            LIBRARY DESTINATION "${CMAKE_INSTALL_LIBDIR}/qml/MatomoQt"
-            ARCHIVE DESTINATION "${CMAKE_INSTALL_LIBDIR}/qml/MatomoQt"
-        )
-    endif()
-
-    get_target_property(matomoqt_qml_module_output_dir MatomoQtQml QT_QML_MODULE_OUTPUT_DIRECTORY)
-    if(matomoqt_qml_module_output_dir)
-        install(
-            DIRECTORY "${matomoqt_qml_module_output_dir}/"
-            DESTINATION "${CMAKE_INSTALL_LIBDIR}/qml/MatomoQt"
-        )
-    endif()
-endif()
-
 install(
     EXPORT MatomoQtTargets
     NAMESPACE MatomoQt::
